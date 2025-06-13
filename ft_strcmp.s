07a13fa9be4b4c7d4abf72485b18123703184338
edu-bm7 <120058703+edu-bm7@users.noreply.github.com.>
2025-06-13 00:22:58 -0300

    comments on ft_strcmp

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -4,7 +4,7 @@ section .text
 ;; It returns a positive number if s1 > s2, negative if s1 < s2 and 0 if they are equal
 ;; s1 is rdi, s2 is rsi
 ft_strcmp:
-    cmp byte [rdi], 0x00
+    cmp byte [rdi], 0
     je .end
     mov dl, [rdi]
     cmp dl, [rsi]
@@ -14,7 +14,7 @@ ft_strcmp:
     jmp ft_strcmp
 
 .end:
-    movzx edx, byte [rdi]
+    movzx edx, byte [rdi] ;; movzx mean mov zero-extend (meaning the upper 32bits will be zero)
     movzx ecx, byte [rsi]
     sub edx, ecx
     mov eax, edx
