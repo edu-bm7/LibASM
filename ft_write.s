3cdb32c1f1bf036870def5bee7cc4564ee7a9af3
edu-bm7 <120058703+edu-bm7@users.noreply.github.com.>
2025-06-13 00:37:47 -0300

    comments on ft_write

--- ft_write.s
+++ ft_write.s
@@ -1,9 +1,10 @@
+;; https://github.com/torvalds/linux/blob/master/arch/x86/entry/syscalls/syscall_64.tbl
 global ft_write
 extern __errno_location
-section .data
-    ;; https://github.com/torvalds/linux/blob/master/arch/x86/entry/syscalls/syscall_64.tbl
-    ;; Number of the `sys_write` system call
-    SYS_WRITE equ 1
+;; Number of the `sys_write` system call outside of data because is constant
+;; And we'll never change it.
+SYS_WRITE equ 1
+
 section .text
 ;; ssize_t write(int fd, const void buf[count], size_t count);
 ;; For a system call the RAX register is the number of that syscall
@@ -16,9 +17,20 @@ ft_write:
     ret
 
 .error:
+    ;; Linux system call conventions return a negative errno by convention
+    ;; So we negate into a positive value it first
     neg rax,
+    ;; we store our errno into EDI register to call the __errno_location
+    ;; and store the address of our errno variable
     mov edi, eax
+    ;; The needs of wrt ..plt has to do with the PIE(Position independent executable)
+    ;; when __errno_location absolute address isn't known at link time, so we load it
+    ;; into the Procedure Linkage Table (PLT) and the dynamic linker will patch it up for us
+    ;; We call it at the PLT stub, otherwise we won't be able to compile our code with -fPIE
+    ;; flags(only working with -no-pie!)
     call __errno_location wrt ..plt
+    ;; RAX now have our C Library errno address (int *)
+    ;; We then save it into the variable in memory *(__errno_location()) = errno
     mov [rax], edi
-    mov rax, -1
+    mov rax, -1 ;; our return value
     ret
