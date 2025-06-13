4fe7e71e8f207032b0746d4088a9d3d70bef19b3
edu-bm7 <120058703+edu-bm7@users.noreply.github.com.>
2025-06-13 00:16:39 -0300

    frame-pointer omission in ft_strlen

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,32 +1,24 @@
 global ft_strlen
 section .text
 ft_strlen:
-    ;; Function prologue
-    ;; Preserve the base pointer
-    push rbp
-    ;; Set the new frame base pointer
-    mov rbp, rsp
+    ;; ft_strlen is a leaf function so we can do frame-pointer omission (no push rbp...)
+    ;; because we don't make any calls to other functions, so we don't need to
+    ;; restore the frame pointer for our callees
+    ;; size_t strlen(const char *s), 
+    ;; We'll return in RAX register the argument in RDI register(first argument)
 
-    ;; size_t strlen(const char *s), so we'll return in rax register
-    ;; the argument on rdi register(first argument)
-
-    ;; First we set our counter register to 0
-    xor rcx, rcx
+    ;; First we set our counter to 0
+    xor rax, rax
 
 .loop:
     ;; We then compare our characters in the string to the `\0` and loop it
     cmp byte [rdi], 0
     je .end
     add rdi, 1
-    add rcx, 1
+    add rax, 1
     jmp .loop
 
 .end:
-    ;; we then mov the value that our counter stored into the rax register
-    mov rax, rcx
-    ;; restore the base pointer
-    ;; Function epilogue
-    pop rbp
     ret
 
 
