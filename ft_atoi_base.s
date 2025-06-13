f5bfca6f984d14f02570954bfe1040098cd144d6
edu-bm7 <120058703+edu-bm7@users.noreply.github.com.>
2025-06-12 22:56:28 -0300

    Improved the comments section with proper alignment and capitalization

--- ft_atoi_base.s
+++ ft_atoi_base.s
@@ -11,63 +11,64 @@ MINUS	equ 45
 
 section .text
 ft_atoi_base:
-    xchg rdi, rsi	    ;; move base to rdi so we can call strlen
-    mov rbx, rdi	    ;; rbx is our scan ptr
+    xchg rdi, rsi           ;; Swap RDI <-> RSI so we can call ft_strlen() with the base.
+    mov rbx, rdi            ;; RBX is our scan ptr.
     call ft_strlen
     cmp rax, 2
     jb .error
-    mov r12, rax	    ;; our base length in callee-saved register
+    mov r12, rax            ;; Our base length in callee-saved register.
     mov rdi, rbx
 
-.valid_base_loop:	    ;; Check if we have a valid base
-    mov r9b, [rbx]
+.valid_base_loop:           ;; Check if we have a valid base.
+    mov r9b, byte [rbx]
     test r9b, r9b
-    je .base_ok		    ;; End of loop NULL
+    je .base_ok             ;; End of loop NULL.
 
-    cmp r9b, PLUS	    ;; forbids '+'
+    cmp r9b, PLUS           ;; Forbids '+'.
     je .error
-    cmp r9b, MINUS	    ;; forbids '-'
+    cmp r9b, MINUS          ;; Forbids '-'.
     je .error
 
-    ;; Forbids range \t <-> /r (spaces)
-    cmp r9b, TAB	    ;; '\t'
+    ;; Forbids spaces
+    cmp r9b, TAB            ;; '\t'
     jb .printable_ok
-    cmp r9b, CR		    ;; '\r'
+    cmp r9b, CR             ;; '\r'
     ja .printable_ok
     jmp .error
 
 .printable_ok:
-    cmp r9b, SPACE	    ;; ' '
+    cmp r9b, SPACE          ;; ' '
     je .error
-    cmp r9b, DEL	    ;; 'DEL'
+    cmp r9b, DEL            ;; 'DEL'
     ja .error
-    mov rcx, rdi	    ;; rdi is our base here
+    mov rcx, rdi            ;; RDI is our base here.
 
 .dup_find_loop:
-    ;; Checks if all characters of rcx already been checked before been equal to first char of rbx
+    ;; Check if all characters of RCX have been checked 
+    ;; when it reaches the first character of RBX.
     cmp rcx, rbx
-    je .dup_not_found	    ;; if so, there isn't duplicates, move to the next rbx
+    je .dup_not_found       ;; If so, there isn't duplicates, move to the next RBX.
     mov al, byte [rcx]
-    cmp al, r9b		    ;; test [rcx] against [rbx] 
-    je .error		    ;; if its equal there is duplicates
+    cmp al, r9b             ;; Test byte [RBX] against byte [RCX].
+    je .error               ;; If its equal there is duplicates.
     add rcx, 1
     jmp .dup_find_loop
 
 .dup_not_found:
-    add rbx, 1		    ;; next base character to test
+    add rbx, 1              ;; Next base character to test.
     jmp .valid_base_loop
 
 .base_ok:
-    xchg rdi, rsi	    ;; revert rdi <-> rsi so base is rdi str is rsi
-    xor r13, r13	    ;; our accumulator variable
-    mov rdx, 1		    ;; our sign variable
+    xchg rdi, rsi           ;; Swap RDI <-> RSI, in a way that base is RDI and, str is RSI.
+    xor r13, r13            ;; Our Accumulator variable.
+    mov rdx, 1              ;; Our Sign variable.
 
 .skip_spaces:
     mov r14b, byte [rdi]
     ;; Skip all spaces before the base
-    cmp r14b, TAB	    ;; '\t'
+    cmp r14b, TAB           ;; '\t'
     jb .not_space
-    cmp r14b, CR	    ;; '\r'
+    cmp r14b, CR            ;; '\r'
     ja .not_space
 
 .is_space:
@@ -75,7 +76,7 @@ ft_atoi_base:
     jmp .skip_spaces
 
 .not_space:
-    cmp r14b, SPACE	    ;; ' '
+    cmp r14b, SPACE         ;; ' '
     je .is_space
 
 ;; Handle multiple signs
@@ -86,35 +87,35 @@ ft_atoi_base:
     jmp .handle_sign
 
 .change_sign:
-    neg rdx		    ;; invert the sign
+    neg rdx                  ;; Invert the sign.
     add rdi, 1
     jmp .handle_sign
 
 .test_negative:
-    cmp byte [rdi], MINUS ;; '-'
+    cmp byte [rdi], MINUS    ;; '-'
     je .change_sign
 
 .atoi_main_loop_preparation:
-    ;; To call find_char_index(base, *str) we need to swap their positions
-    xchg rdi, rsi	    ;; rsi <-> rdi
-    mov r8, rdi		    ;; save rdi to r8 so we can restore our base later
+    ;; To call find_char_index(base, *str)
+    ;; we need to swap their positions.
+    xchg rdi, rsi           ;; RSI <-> RDI.
+    mov r8, rdi             ;; Save RDI in R8 so we can restore our base later.
 
 .outer_loop:
-    mov rdi, r8		    ;; restore the base to continue the loop
-    xor rcx, rcx	    ;; zero our counter
-    ;; move the rsi character into r9b to test if we reach the end of string \0
-    mov r9b, byte [rsi] 
+    mov rdi, r8             ;; Restore the base to continue the loop.
+    xor rcx, rcx            ;; Zero out our counter.
+    mov r9b, byte [rsi]     ;; Test if we reached end of string ('\0').
     test r9b, r9b
     jz .finish
 
 .next_digit:
-    mov rbx, rsi	    ;; RBX is our base pointer, that we'll use in our loop
+    mov rbx, rsi            ;; RBX is our base pointer, that we'll use in our loop.
 
 .find_char:
-    mov r10b, byte [rdi]    ;; test if we already checked all our base
+    mov r10b, byte [rdi]    ;; Rest if we reached end of base string ('\0').
     test r10b, r10b
     jz .finish
-    mov al, byte [rbx]	    ;; test if our str found an index in the base
+    mov al, byte [rbx]      ;; Test if our str found an index in the base.
     test al, al
     jz .not_found
     cmp al, r10b
@@ -124,8 +125,8 @@ ft_atoi_base:
     jmp .find_char
 
 .found:
-    imul r13, r12	    ;; multiply our accumulator * base_length
-    add r13, rcx	    ;; adds the base index to our accumulator
+    imul r13, r12           ;; Multiply our accumulator * base_length.
+    add r13, rcx            ;; Adds the base index to our accumulator.
     add rsi, 1
     jmp .outer_loop
 
@@ -136,10 +137,10 @@ ft_atoi_base:
     jmp .next_digit
 
 .finish:
-    imul r13, rdx	    ;; multiply our accumulator * sign
-    mov rax, r13	    ;; move it to the rax register
+    imul r13, rdx           ;; Multiply our accumulator * sign.
+    mov rax, r13            ;; Move it to the RAX register.
     ret
 
 .error:
-    xor rax, rax	    ;; zero the rax register
+    xor rax, rax            ;; Zero out the RAX register on error.
     ret
