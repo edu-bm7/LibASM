f59a62c1d892e7b1f5986dc47d27ddaf22ff3760
edu-bm7 <120058703+edu-bm7@users.noreply.github.com.>
2025-06-13 00:46:56 -0300

    comments on ft_strdup

--- ft_strdup.s
+++ ft_strdup.s
@@ -6,6 +6,8 @@ extern malloc
 
 section .text
 ft_strdup:
+    ;; Since we need to call other functions inside this one, we can't omit the
+    ;; frame-pointer
     ;; Function Prologue
     ;; Preserve base pointer
     push rbp
@@ -13,28 +15,30 @@ ft_strdup:
     mov rbp, rsp
     ;; Reserve stack space for local variable
     sub rsp, 8
-    mov [rbp-8], rdi ;; save rdi (our string) into local variable because malloc change the registers
+    mov [rbp-8], rdi           ;; Save RDI (our string) into local variable
+                               ;; because malloc change the caller-saved scratch registers.
     call ft_strlen
-    add rax, 1 ;; space for null-terminator
-    mov rdi, rax ;; prepare for malloc call
+    add rax, 1                 ;; Space for null-terminator.
+    mov rdi, rax               ;; Prepare for malloc call.
     call malloc wrt ..plt
-    ;; xor rax, rax  ;; Uncomment this instruction to test malloc failure
+    ;; xor rax, rax            ;; Uncomment this instruction to test malloc failure.
     test rax, rax
     jz .error
     mov rdi, rax
-    mov rsi, [rbp-8] ;; pass the variable to the rsi register so we can call strcpy
+    mov rsi, [rbp-8]
     call ft_strcpy
-    add rsp, 8 ;; pop back the space taken by the variable, maintaining the stack aligned
+    add rsp, 8                 ;; Pop back the space taken by the variable,
+                               ;; maintaining the stack aligned.
     ;; Function Epilogue
     pop rbp
     ret
 
 .error:
-    mov edi, 12 ;; errno = (ENOMEM)
+    mov edi, 12                 ;; errno = (ENOMEM)
     call __errno_location wrt ..plt
-    mov [rax], edi ;; write in memory the value of errno
-    xor rax, rax ;; return NULL on error
-    add rsp, 8 ;; alignment
+    mov [rax], edi
+    xor rax, rax
+    add rsp, 8
     ;; Function Epilogue
     pop rbp
     ret
